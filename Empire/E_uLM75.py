992a3f8b7f246401f6621dc6908e159331a4d6d7
Kaspar Kallas <kaspar@digitalsputnik.com>
2021-11-15 21:11:21 +0200

    Update E_uLM75.py
    added timer execution for automatic updates

--- Empire/E_uLM75.py
+++ Empire/E_uLM75.py
@@ -1,33 +1,45 @@
 from math import floor
 import time
+import machine
 
 class E_uLM75(object):
     _address = 0x4f
     _error = "None"
     _last_results = (0,0)
     _last_read = 0
+    _currentTemp = 0
+    _tempValues = []
 
     def __init__(self, i2cInput, address=0x4f):
         self.i2c = i2cInput
         self._address = address
+        
+        self._tempTimer = machine.Timer(2)
+        self._tempTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=self._updateTemp) 
 
     def _get_output(self):
         """Return raw output from the LM75 sensor."""
         output = self.i2c.readfrom(self._address, 2)
         return output[0], output[1]
 
-    def get_temp(self):
+    def _get_temp(self):
         """Return a tuple of (temp_c, point)."""
-        if time.ticks_add(self._last_read, 500)<time.ticks_ms():
-            try:
-                temp = self._get_output()
-                self._last_results = (int(temp[0]), floor(int(temp[1]) / 23))
-                return self._last_results[0], self._last_results[1]
-            except(E):
-                self._error = E
-                return 0,0
+        try:
+            temp = self._get_output()
+            self._last_results = (int(temp[0]), floor(int(temp[1]) / 23))
+            return self._last_results[0]*10+self._last_results[1]
+        except(E):
+            self._error = E
+            return 0.0
+        
+    def _updateTemp(self,caller):
+        if len(self._tempValues) < 7:
+            self._tempValues.append(self._get_temp())
         else:
-            self._error = "To fast read, sensor can be read 2x a sec"
-            return self._last_read
+            self._currentTemp = self._tempValues[3]
+            self._tempValues = []
+            
+    def read(self):
+        return self._currentTemp
             
         
